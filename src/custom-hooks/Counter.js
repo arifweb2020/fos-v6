fda799f24ed1e1bd27a25098aaab996c0f4b8f56
arifweb2020 <arifhussainb2019@gmail.com>
2023-01-21 22:05:17 +0530

    custom-hooks

---
+++ src/custom-hooks/Counter.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function useCounter(props) {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount((prev) => prev + 1)
+    }
+
+    const decrement = () => {
+        setCount((prev) => prev - 1)
+    }
+
+    return {
+        count,
+        increment,
+        decrement
+    }
+}
+
+export default useCounter;
